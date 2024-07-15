b2dd75eaa0aea3b15f5057e3ed265c2f540e45cb
keita.mitsuhashi <kmdatg@gmail.com>
2024-07-15 11:05:30 +0900

    fix: 管理画面タイトルの間違いを修正

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
   <x-slot name="header">
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          {{ __('Dashboard') }}
+          フォーム
       </h2>
   </x-slot>
   <div class="max-w-7xl mx-auto px-6">
